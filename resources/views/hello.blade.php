d219031a1d11559bafa5aec7c4e7787d7e116b07
alifirhas <irhasalif@gmail.com>
2021-02-18 20:52:00 +0700

    blade intro

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -7,6 +7,28 @@
 @section('content')
 
     welcome to Home
+    
+    <p></p>
+    
+    <?php 
+        $makan = "ini variable dalam satu dokumen";
+        $con = true;
+    ?>
+
+    <p></p>
+
+    this is using data from routes
+    <br>the name is {{$name}}
+    <br>panggil {{$makan}}
+    <br>ini fungsi dalam php {{date("Y/m/d")}}
+
+    {{-- 
+        
+        bisa panggil variable yang dikirim dari routes/web.php atau satu file pakai {{$namaVariable}}
+        fungsi dari php juga bisa dipanggil pakai {{ }}
+        dan sekalian ini cara beri komentar di blade
+
+    --}}
 
 @endsection
 
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', function () {
-    return view('hello');
+    return view('hello', ['name' => "alif"]);
 });
 
 Route::get('/posts', function () {
